fe54da93a451247a42e604f6fcd1469a8eb3886e
Mukul Jangra <148455498+MUKUL6121@users.noreply.github.com>
2025-11-26 19:10:31 +0530

    Update file upload form action URL

--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -3,7 +3,7 @@ export default function form() {
         <>
             <h2>Upload a File</h2>
             <br />
-            <form id="upload Form" action="http://localhost:3000/upload" class="border border-1 border-black p-3" method="post" enctype="multipart/form-data">
+            <form id="upload Form" action="https://file-upload-backend-htbh.onrender.com/upload" class="border border-1 border-black p-3" method="post" enctype="multipart/form-data">
                 <input type="file" name="samplefile" required multiple />
                 <button type="submit">Upload</button>
             </form>
